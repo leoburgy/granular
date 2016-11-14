43fff306a4376cdb267245b301827422e05a06bb
Whan, Alex (PI, Black Mountain) <Alex.Whan@csiro.au>
2016-11-14 12:30:29 +1100

    updated docs for data

---
+++ R/data.R
@@ -0,0 +1,67 @@
+#' An distribution data set
+#' 
+#' A dataset containing the distributions from three samples
+#' 
+#' @format A data frame with 100 rows and 4 variables:
+#' \describe{
+#' \item{size}{Size of granule, in um}
+#' \item{118 - Average}{Distribution of sample}
+#' \item{206 - Average}{Distribution of sample}
+#' \item{85 - Average}{Distribution of sample}
+#' }
+#' 
+"Dist"
+
+#' An example mix_dist output
+#' 
+#' A dataset containing the output from 118 - Average
+#' 
+#' @format A data frame with 3 rows and 8 variables:
+#' \describe{
+#' \item{sample}{Name of the sample}
+#' \item{peak}{Peak name}
+#' \item{pi}{Distribution proportion}
+#' \item{mu}{Distribution mean}
+#' \item{sigma}{Distribution sd}
+#' \item{pi.se}{proportion standard error}
+#' \item{mu.se}{mean standard error}
+#' \item{sigma.se}{sd standard error}
+#' }
+#' 
+"ms1"
+
+#' An example mix_dist output
+#' 
+#' A dataset containing the output from 85 - Average
+#' 
+#' @format A data frame with 3 rows and 8 variables:
+#' \describe{
+#' \item{sample}{Name of the sample}
+#' \item{peak}{Peak name}
+#' \item{pi}{Distribution proportion}
+#' \item{mu}{Distribution mean}
+#' \item{sigma}{Distribution sd}
+#' \item{pi.se}{proportion standard error}
+#' \item{mu.se}{mean standard error}
+#' \item{sigma.se}{sd standard error}
+#' }
+#' 
+"ms2"
+
+#' An example mix_dist output
+#' 
+#' A dataset containing the output from 206 - Average
+#' 
+#' @format A data frame with 3 rows and 8 variables:
+#' \describe{
+#' \item{sample}{Name of the sample}
+#' \item{peak}{Peak name}
+#' \item{pi}{Distribution proportion}
+#' \item{mu}{Distribution mean}
+#' \item{sigma}{Distribution sd}
+#' \item{pi.se}{proportion standard error}
+#' \item{mu.se}{mean standard error}
+#' \item{sigma.se}{sd standard error}
+#' }
+#' 
+"ms3"
