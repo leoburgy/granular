d99b9eac4ea35ceb63562856bb7cc05c2112becb
Whan, Alex (PI, Black Mountain) <Alex.Whan@csiro.au>
2016-11-14 15:28:49 +1100

    added function run_granular. closes #10

--- R/run_shiny.R
+++ R/run_shiny.R
@@ -1,12 +1,20 @@
 #' Run the granular shiny app locally
 #'
-#' @export
+#' @param port The TCP port that the application should listen on. Defaults to choosing a random port. 
+#' @param launch.browser If true, the system's default web browser will be launched automatically after the app is started. defaults to true in interactive sessions only.
+#' @param host The IPv4 address that the application should listen on. Defaults
+#'   to the \code{shiny.host} option, if set, or \code{"127.0.0.1"} if not.
 #'
-run_granular <- function() {
-  appDist <- system.file("shiny", "granular", package = "granular")
-  if(appDit == "") {
+#' @export
+run_granular <- function(port = NULL, 
+                         launch.browser = getOption("shiny.launch.browser", 
+                                                    interactive()),
+                         host=getOption('shiny.host', '127.0.0.1')) {
+  appDir <- system.file("shiny", "granular", package = "granular")
+  if(appDir == "") {
     stop("Could not find example directory. Try re-installing 'granular'.", call. = FALSE)
+  } else {
+    shiny::runApp(appDir, port = port, host = host,launch.browser = launch.browser,  display.mode = "normal")
   }
   
-  shiny::runApp(appDir, display.mode = "normal")
 }
