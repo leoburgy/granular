3903da65d30f1108c9d87ba5da3f263e2096a194
Gaire, Raj (Data61, Acton) <Raj.Gaire@csiro.au>
2016-11-15 17:04:14 +1100

    fix zoom in problem #8

--- inst/shiny/granular/www/plotter.js
+++ inst/shiny/granular/www/plotter.js
@@ -3,10 +3,10 @@
 		var networkOutputBinding = new Shiny.OutputBinding();
 
 		$.extend(networkOutputBinding, {
-			lines : 		{"counter":0, "brushed": false},
+			lines : 		null,
 			lineHeight : 	40,
-			margin : 		{top: 20, right: 80, bottom: 30, left: 50},
-			width : 		500, 
+			margin : 		{top: 20, right: 10, bottom: 30, left: 0},
+			width : 		600, 
 			height: 		400,
 			initialised: 	false,
 			brush : 		null,
@@ -25,40 +25,47 @@
 			hoverText: 		null,
 			xAxis: 			null,
 			yAxis: 			null,
+			gX: 			null,
+			gY: 			null,
 			init: function(){
 				if(this.initialised) return;
-				this.brush = d3.brushX().extent([[0, 0], [this.width, this.height]]).on("end", this.brushended.bind(this));
+				// this.width = this.width - this.margin.left - this.margin.right;
+ 				// this.height = this.height - this.margin.top - this.margin.bottom;
+				this.brush = d3.brushX().extent([[0, 0], [this.width + this.margin.left + this.margin.right, this.height + this.margin.top + this.margin.bottom]])
+					.on("end", this.brushended.bind(this));
 				this.x = d3.scaleLog().range([0, this.width]);
 				this.x0 = d3.scaleLog().range([0, this.width]);
 				this.y = d3.scaleLinear().range([this.height, 0]); 
 				this.z = d3.scaleOrdinal(d3.schemeCategory10);
 				this.line = d3.line().curve(d3.curveBasis)
-					.x( (function(d) { return this.x(d.psize); }).bind(this))
+					.x( (function(d) { return this.x(d.psize) < this.margin.left ? null : this.x(d.psize); }).bind(this))
 					.y( (function(d) { return this.y(d.frequency); }).bind(this));
-				this.xAxis = d3.axisTop(this.x); 
+				this.xAxis = d3.axisBottom(this.x).ticks(5).tickFormat(d3.formatPrefix(".1", 1e1)); 
 				this.yAxis = d3.axisRight(this.y);
 
 				this.initalised = true;
 			},
 			drawGraph: function(data){
 				var g = this.svg.append("g").attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
-				g = this.svg.append("g");
-
-				g.append("g")
+				this.gX = g.append("g")
 					.attr("class", "axis axis--x")
 					.call(this.xAxis)
-					.attr("transform", "translate(0," + (this.height) + ")")
-					.append("text")
-					.attr("fill", "#000")
-					.text("Particle Size");
+					.attr("transform", "translate(0," + this.height + ")");
 
-				g.append("g")
+				this.gX.append("text")
+					.attr("fill", "#000")
+					.text("Particle Size")
+					.attr("x", this.width - 30)
+					.attr("y", 30);
+
+				this.gY = g.append("g")
 					.attr("class", "axis axis--y")
-					.call(this.yAxis)
-					.append("text")
-					//.attr("transform", "rotate(-90)")
+					.call(this.yAxis);
+					
+				this.gY.append("text")
+					.attr("transform", "rotate(-90)")
 					.attr("y", 15)
-					.attr("x",10)
+					.attr("x", -50)
 					.attr("dy", "0.71em")
 					.attr("fill", "#000")
 					.text("Frequency");
@@ -77,12 +84,9 @@
 				
 				// Add mouseover events.
 				d3.select("svg").on("mousemove", function() {
-					var mouse_x = d3.mouse(this)[0];
-					var graph_x = Math.round(that.x.invert(mouse_x)*100)/100;
-				
 					if(that.lines.counter >= 3 || !that.lines.brushed) return;
 					var mouse_x = d3.mouse(this)[0];
-					var graph_x = Math.round(that.x.invert(mouse_x)*100)/100;
+					var graph_x = Math.round(that.x.invert(mouse_x - that.margin.left)*100)/100;
 
 					var char = "A", counter = 0;
 					if(!that.lines.A){char="A"; }
@@ -100,7 +104,7 @@
 				}).on('click', function(){
 					if(that.lines.counter >= 3 || !that.lines.brushed) return;
 					var mouse_x = d3.mouse(this)[0];
-					var graph_x = Math.round(that.x.invert(mouse_x)*100)/100;
+					var graph_x = Math.round(that.x.invert(mouse_x - that.margin.left)*100)/100;
 
 					var lineGroup = that.svg.append("g").attr("class", "hover-line");
 
@@ -112,7 +116,8 @@
 					var line = lineGroup
 						.append("line")
 						.attr("x1", mouse_x).attr("x2", mouse_x) 
-						.attr("y1", 0).attr("y2", that.height); 
+						.attr("y1", that.margin.top/2).attr("y2", that.height + that.margin.top/2 + that.margin.bottom/2); 
+						// .attr("y1", 0).attr("y2", that.height); 
 
 					var text = lineGroup.append('text')
 					.attr("class", "hover-text")
@@ -133,23 +138,28 @@
 			brushended: function() {
 				var s = d3.event.selection;
 
-				//Only brush once
-				if(this.lines.brushed) return;
-
 				if (!s) {
 					console.log("not s");
 					return;
 				} else {
-					console.log("yes s:" + s);
-					min = s[0];
-					max = s[1];
-					this.x.domain([s[0],s[1]].map(this.x.invert, this.x));
+					console.log("yes s:" + s+ " scale "+ this.width / (s[1] - s[0]));
+					this.x.domain([s[0] - this.margin.left, s[1] - this.margin.left ].map(this.x.invert, this.x));
+					this.lines.min = this.x0.invert(s[0]);
+					this.lines.max = this.x0.invert(s[1]);
+					
 					Shiny.onInputChange("min_val", this.x0.invert(s[0]));
 					Shiny.onInputChange("max_val", this.x0.invert(s[1]));
 					this.svg.select(".brush").call(this.brush.move, null);
 				}
 				this.lines.brushed = true;
 				this.zoom();
+			},
+			brushing: function(){
+				if(this.lines.brushed) return;
+				var mouse_x = d3.mouse(this)[0];
+				var graph_x = Math.round(that.x.invert(mouse_x - that.margin.left)*100)/100;
+				console.log(graph_x);
+
 			},
 			zoom: function () {
 				var t = this.svg.transition().duration(750);
@@ -159,24 +169,27 @@
 					.attr("d", (function(d) { return this.line(d.values); }).bind(this));
 			},
 			find: function(scope) {
-				this.init();
 				return $(scope).find('.shiny-network-output');
 			},
 			renderValue: function(el, data) {
 				d3.selectAll(".svg-container").remove();
+				this.lines = {"counter":0, "brushed": false};
 				if(!data) return;
-				d3.select("div#mastersizer")
+				this.svg = d3.select("div#mastersizer")
 					.append("div")
 					.classed("svg-container", true) //container class to make it responsive
-					.append("svg")
-					//responsive SVG needs these 2 attributes and no width and height attr
-					.attr("preserveAspectRatio", "xMinYMin meet")
-					.attr("viewBox", "0 0 600 400")
+					.append("svg");
+					//.attr("width", this.width + this.margin.left + this.margin.right)
+    				//.attr("height", this.height + this.margin.top + this.margin.bottom);
+				//responsive SVG needs these 2 attributes and no width and height attr
+				this.svg.attr("preserveAspectRatio", "xMinYMin meet")
 					//class to make it responsive
 					.classed("svg-content-responsive", true);
 				
 				// Initialise SVG and related variables
 				this.svg = d3.select("svg");
+
+				this.init();
 				this.svg.selectAll("#sample").remove();
 				this.svg.append("g")
 					.attr("class", "brush")
@@ -189,7 +202,7 @@
 				this.hoverLine = this.hoverLineGroup
 					.append("line")
 					.attr("x1", 0).attr("x2", 0) 
-					.attr("y1", 0).attr("y2", this.height); 
+					.attr("y1", this.margin.top/2).attr("y2", this.height + this.margin.top/2 + this.margin.bottom/2); 
 
 				this.hoverText = this.hoverLineGroup.append('text')
 					.attr("class", "hover-text")
@@ -208,7 +221,6 @@
 					}
 					this.samples.push(myobj);
 				}
-
 				this.x.domain(d3.extent(data.size, function(d) { return d; }));
 				this.x0.domain(d3.extent(data.size, function(d) { return d; }));
 
@@ -217,6 +229,8 @@
 					d3.max(this.samples, function(c) { return d3.max(c.values, function(d) { return d.frequency; }); })
 				]);
 				this.z.domain(this.samples.map(function(c) { return c.id; }));
+
+				this.svg.attr("viewBox", "0 0 "+ (this.width + this.margin.left + this.margin.right) +" "+ (this.height + this.margin.top + this.margin.bottom));
 
 				this.drawGraph(data);
 			}
