25909f98581a148b50ffaf84999206b96d1c74d8
Whan <wha022@csiro.au>
2016-11-22 23:18:05 +1100

    navbar positioned properly, but change of tabset id means navigation and hiding is broken

--- inst/shiny/granular/ui.R
+++ inst/shiny/granular/ui.R
@@ -7,54 +7,51 @@ mastersizer_vis <- function(outputId) {
 
 navbarPage(
   title = "Starch Granule Mixture Classification",
-  tabsetPanel(
-    id = "tabset",
-    useShinyjs(),
-    tags$head(
-      tags$script(src="//d3js.org/d3.v4.js"),
-      tags$link(rel = 'stylesheet', type = 'text/css', href = 'custom.css')
-    ),
-    #First tabPanel for visualisation of data to estimate number of mixtures and where the centres are
-    tabPanel(title = "Setup", value = "setup",
-             #sidebarPanel for data upload, 
-             tags$div(id = "sidePanel", class = 'col-sm-4',
-                      tags$form(class = 'well',
-                                tags$div(class = 'row-fluid',
-                                         fileInput('file', 'Choose CSV file', accept=c('text/csv', 
-                                                                                       'text/comma-separated-values,text/plain', 
-                                                                                       '.csv'))),
-                                tags$div(class = 'row-fluid',
-                                         selectInput("select_data", "Select data", c("Single sample", "Three samples", "Thirty-six samples"))),
-                                tags$div(class = 'row-fluid',
-                                         checkboxInput('use_example', 'Use example data')),
-                                tags$div(class = 'row-fluid',
-                                         downloadLink('download_example', "Download example data"))
-                      ),
-                      tags$form(id = 'guide', class = 'well',
-                                tags$div(h4("Guide:"), 
-                                         p(id = "step1", class = "inst", "1. Select Data"),
-                                         p(id = "step2", class = "inst", "2. Click and drag on the plot to identify the area with true peaks"),
-                                         p(id = "step3", class = "inst", "3. Click to identify peaks")),
-                                actionButton("goButton", "4. Process Data", width = '100%')
-                      ),
-                      tags$form(id = 'restart', class = 'well',
-                                actionButton("restartButton", "Start again", width = '100%'))
-             ),
-             mainPanel(
-               textOutput("granular_version"),
-               tags$script(src="plotter.js"),
-               mastersizer_vis("mastersizer")
-             )
-    ),
-    tabPanel(title = "Output", value = "output",
-             shiny::dataTableOutput("longDataTable")
-    ),
-    tabPanel(title = "Summary", value = "summary"
-    ),
-    tabPanel(title = "Plots", value = "plots",
-             div(class = "center-button",
-                 downloadButton("downloadPlot", "Download all fit plots as zip", class = "center-button")
-             )
-    )
+  #First tabPanel for visualisation of data to estimate number of mixtures and where the centres are
+  tabPanel(title = "Setup", value = "setup",
+           useShinyjs(),
+           tags$head(
+             tags$script(src="//d3js.org/d3.v4.js"),
+             tags$link(rel = 'stylesheet', type = 'text/css', href = 'custom.css')
+           ),
+           #sidebarPanel for data upload, 
+           tags$div(id = "sidePanel", class = 'col-sm-4',
+                    tags$form(class = 'well',
+                              tags$div(class = 'row-fluid',
+                                       fileInput('file', 'Choose CSV file', accept=c('text/csv', 
+                                                                                     'text/comma-separated-values,text/plain', 
+                                                                                     '.csv'))),
+                              tags$div(class = 'row-fluid',
+                                       selectInput("select_data", "Select data", c("Single sample", "Three samples", "Thirty-six samples"))),
+                              tags$div(class = 'row-fluid',
+                                       checkboxInput('use_example', 'Use example data')),
+                              tags$div(class = 'row-fluid',
+                                       downloadLink('download_example', "Download example data"))
+                    ),
+                    tags$form(id = 'guide', class = 'well',
+                              tags$div(h4("Guide:"), 
+                                       p(id = "step1", class = "inst", "1. Select Data"),
+                                       p(id = "step2", class = "inst", "2. Click and drag on the plot to identify the area with true peaks"),
+                                       p(id = "step3", class = "inst", "3. Click to identify peaks")),
+                              actionButton("goButton", "4. Process Data", width = '100%')
+                    ),
+                    tags$form(id = 'restart', class = 'well',
+                              actionButton("restartButton", "Start again", width = '100%'))
+           ),
+           mainPanel(
+             textOutput("granular_version"),
+             tags$script(src="plotter.js"),
+             mastersizer_vis("mastersizer")
+           )
+  ),
+  tabPanel(title = "Output", value = "output",
+           shiny::dataTableOutput("longDataTable")
+  ),
+  tabPanel(title = "Summary", value = "summary"
+  ),
+  tabPanel(title = "Plots", value = "plots",
+           div(class = "center-button",
+               downloadButton("downloadPlot", "Download all fit plots as zip", class = "center-button")
+           )
   )
 )
