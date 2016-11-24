7f6b48044e8f7e2dd9d2f210ac1d8a2b7d3f90d6
Alex Whan <alexwhan@gmail.com>
2016-11-25 00:17:44 +1100

    removing outdated shiny test

--- tests/testthat/test_shiny.R
+++ tests/testthat/test_shiny.R
@@ -1,9 +1,9 @@
-library(granular)
-library(shinytest)
-
-context("shiny initialisation")
-
-test_that("shiny app launches", {
-  app <- shinytest::shinyapp$new("granular")
-  expect_false(app$get_value("use_example"))
-})
+# library(granular)
+# library(shinytest)
+# 
+# context("shiny initialisation")
+# 
+# test_that("shiny app launches", {
+#   app <- shinytest::shinyapp$new("granular")
+#   expect_false(app$get_value("use_example"))
+# })
