fdef2280278db364e1d9f01fca4a636eb1f881c1
Whan <wha022@csiro.au>
2016-11-24 22:15:23 +1100

    shiny tests... working?

--- tests/testthat/test_shiny.R
+++ tests/testthat/test_shiny.R
@@ -1,16 +1,10 @@
-# library(granular)
-# library(shinytest)
-# 
-# context("shiny initialisation")
-# 
-# test_that("shiny app launches", {
-#   app <- shinytest::shinyapp$new("granular")
-#   # expect_false(app$get_value("use_example"))
-#   # print(list.files("granular"))
-# })
-# 
-# test_that("shiny app launches2", {
-#   app <- shinytest::shinyapp$new("apps/081-widgets-gallery/")
-#   # expect_false(app$get_value("use_example"))
-#   # print(list.files("../../inst/shiny/granular"))
-# })
+library(shinytest)
+library(shinyjs)
+context("shinyapp")
+
+app <- shinytest::shinyapp$new("../../granular/shiny/granular/")
+
+test_that("get_value", {
+  expect_false(app$get_value("use_example"))
+})
+
