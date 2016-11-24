75207518219225632c03d38606f58543b92b8979
Whan <wha022@csiro.au>
2016-11-24 21:33:38 +1100

    trying to get app working under /tests/testthat

---
+++ tests/testthat/granular/server.R
@@ -0,0 +1,221 @@
+library(shiny)
+library(tidyr)
+library(dplyr)
+library(scales)
+library(shinyjs)
+library(ggplot2)
+library(granular)
+# source('../../../R/granular.R')
+
+shinyServer(function(input, output, session) {
+
+  # values <- reactiveValues()
+  # 
+  # granular <- reactiveValues()
+  # 
+  # observe({
+  #   granular$version <- sessionInfo()
+  # })
+  # 
+  # 
+  # output$granular_version <- renderText({
+  #   paste0("granular - v", granular$version$otherPkgs$granular$Version)
+  # })
+  # 
+  # observe({
+  #   toggle('select_data', condition = input$use_example)
+  #   toggle('file', condition = !input$use_example)
+  #   toggle('download_example', condition = input$use_example)
+  #   params <- values$params
+  # 
+  #   #Set class toggle for instruction text
+  #   toggleClass('step1', "instgrey", !is.null(getData()))
+  #   toggleClass('step2', "instgrey", (is.null(getData()) | !any(as.logical(lapply(params[[1]], is.null)))))
+  #   toggleClass('step3', "instgrey", (is.null(getData()) | any(as.logical(lapply(params[[1]], is.null))) | all(!as.logical(lapply(params[[2]], is.null)))))
+  # 
+  #   #Set toggle for the go button
+  #    toggleState('goButton', condition = !any(as.logical(lapply(params[[2]], is.null))))
+  # })
+  # 
+  # observe({
+  #   #Hide tabs to start
+  #   hide(selector = "#tabset li a[data-value=output]")
+  #   hide(selector = "#tabset li a[data-value=summary]")
+  #   hide(selector = "#tabset li a[data-value=plots]")
+  # })
+  # 
+  # observe({
+  #   input$restartButton
+  #   isolate({
+  #     reset("sidePanel")
+  #     reset("goButton")
+  #     values$params <- list(range = list(min_val = NULL, max_val = NULL),
+  #                           peaks = list(peak_A = NULL, peak_B = NULL, peak_C = NULL))
+  #     updateTabsetPanel(session, "tabset", "setup")
+  #     hide(selector = "#tabset li a[data-value=output]")
+  #     hide(selector = "#tabset li a[data-value=summary]")
+  #     hide(selector = "#tabset li a[data-value=plots]")
+  #   })
+  # })
+  # 
+  # output$download_example <- downloadHandler(
+  #   filename = switch(input$select_data,
+  #                     "Single sample" = "singleMastersizer.csv",
+  #                     "Three samples" = "tripleMastersizer.csv",
+  #                     "Thirty-six samples" = "extdata/fullMastersizer.csv"),
+  #   content = function(file) {
+  #     write.csv(read.csv(example_file(), check.names = FALSE), file, row.names = FALSE)
+  #   }
+  # )
+  # 
+  # example_file <- reactive({
+  #   switch(input$select_data,
+  #          "Single sample" = "../../extdata/singleMastersizer.csv",
+  #          "Three samples" = "../../extdata/tripleMastersizer.csv",
+  #          "Thirty-six samples" = "../../extdata/fullMastersizer.csv")
+  # })
+  # 
+  # getFile <- reactive({
+  #   input$restartButton
+  #   if(!input$use_example) {
+  #     inFile <- input$file$datapath
+  #   } else {
+  #     inFile <- example_file()
+  #   }
+  # 
+  #   if(is.null(inFile)) return(NULL)
+  #   return(inFile)
+  # })
+  # 
+  # getData <- reactive({
+  #   inFile <- getFile()
+  #   if(is.null(inFile)) return(NULL)
+  #   wideData <- read.csv(inFile)
+  #   tData <- gather(wideData, size, proportion, -sample) %>%
+  #     mutate(size = as.numeric(sub("X", "", size))) %>%
+  #     spread(sample, proportion)
+  #   return(tData)
+  # })
+  # 
+  # output$mastersizer <- reactive({
+  #   getData()
+  # })
+  # 
+  # observe({
+  #   min_val <- if(!is.null(input$min_val)) {
+  #     input$min_val
+  #   } else NULL
+  # 
+  #   max_val <- if(!is.null(input$max_val)) {
+  #     input$max_val
+  #   } else NULL
+  # 
+  #   npeaks <- if(!is.null(input$npeaks)) {
+  #     input$npeaks
+  #   } else NULL
+  # 
+  #   peak_vals <- if(!is.null(npeaks)) {
+  #     peak_ids <- LETTERS[1:npeaks]
+  #     peak_vals <- eval(parse(paste0("input$peak_", peak_ids)))
+  #   }
+  # 
+  #   peak_A <- if(!is.null(input$peak_A)) {
+  #     input$peak_A
+  #   } else(NULL)
+  # 
+  #   peak_B <- if(!is.null(input$peak_B)) {
+  #     input$peak_B
+  #   } else(NULL)
+  # 
+  #   peak_C <- if(!is.null(input$peak_C)) {
+  #     input$peak_C
+  #   } else(NULL)
+  # 
+  #   values$params <- list(range = list(min_val = min_val, max_val = max_val),
+  #                         peaks = list(peak_A = peak_A, peak_B = peak_B, peak_C = peak_C))
+  #   })
+  # 
+  # filteredData <- reactive({
+  #   if(is.null(values$params[[1]][[1]])) {
+  #     return(NULL)
+  #   }
+  # 
+  #   if(is.null(getData())) return(NULL)
+  # 
+  #   params <- values$params
+  #   tData <- getData()
+  # 
+  #   outData <- tData %>%
+  #     gather(sample, proportion, -size) %>%
+  #     filter(size > params[[1]][[1]],
+  #            size < params[[1]][[2]]) %>%
+  #     spread(sample, proportion)
+  #   return(outData)
+  # })
+  # 
+  # observe({
+  #   if(input$goButton == 0)
+  #     return(NULL)
+  #   isolate({tData <- filteredData()
+  #           params <- values$params
+  #           means <- rev(unlist(params[[2]]))
+  #           ps <- tData[[1]]
+  #           n <- ncol(tData)
+  #           output_list <- vector("list", n - 1)
+  #           withProgress({
+  #             for(i in seq_len(n - 1)) {
+  #               incProgress(1/n,
+  #                           "Calculating...",
+  #                           paste("working on",
+  #                                 names(tData)[i + 1],
+  #                                 "which is",
+  #                                 i, "of", n - 1)
+  #               )
+  #               newfit <- granular::mix_dist(tData[[i + 1]], ps,
+  #                                            names(tData)[i + 1], comp_means = means)
+  #               output_list[[i]] <- newfit[[1]]
+  #             }
+  #           })
+  #           values$output_list <- output_list
+  #           toggle(selector = "#tabset li a[data-value=output]")
+  #           toggle(selector = "#tabset li a[data-value=summary]")
+  #           toggle(selector = "#tabset li a[data-value=plots]")
+  #           updateTabsetPanel(session, "tabset", "output")
+  #           return(output_list)})
+  # })
+  # 
+  # output$longDataTable <- renderDataTable({
+  #   if(is.null(values$output_list)) return(NULL)
+  #   bind_rows(values$output_list)
+  # })
+  # 
+  # output$downloadPlot <- downloadHandler(
+  #   filename = "granular_plots.zip",
+  #   content = function(file) {
+  #     if(is.null(values$output_list)) {
+  #       return(NULL)
+  #     } else {
+  #       output_list <- values$output_list
+  #       output_plots <- vector("list", length(output_list))
+  #       tmpdir <- tempdir()
+  #       tData <- filteredData()
+  #       ps <- tData[[1]]
+  #       for(i in seq_len(length(output_list))) {
+  #         output_plots[[i]] <- paste0(names(tData)[[i + 1]], ".png")
+  #         ggsave(paste0(tmpdir, "/", output_plots[[i]]),
+  #                granular:::ggfit(output_list[[i]],
+  #                                 tData[[i + 1]],
+  #                                 ps,
+  #                                 title = names(tData)[i + 1]),
+  #                device = "png",
+  #                width = 6, height = 6)
+  #       }
+  #       wd <- getwd()
+  #       setwd(tmpdir)
+  #       zip(file, paste0(unlist(output_plots)))
+  #       setwd(wd)
+  #     }
+  #   }
+  # )
+})
+
---
+++ tests/testthat/granular/ui.R
@@ -0,0 +1,68 @@
+library(shiny)
+library(shinyjs)
+library(rmarkdown)
+
+mastersizer_vis <- function(outputId) {
+  HTML(paste("<div id=\"", outputId, "\" class=\"shiny-network-output\"></div>", sep=""))
+}
+
+navbarPage(
+  title = "title"#textOutput("granular_version"),
+  # id = "tabset",
+  # windowTitle = "granular",
+  # #First tabPanel for visualisation of data to estimate number of mixtures and where the centres are
+  # tabPanel(title = "Setup", value = "setup",
+  #          useShinyjs(),
+  #          tags$head(
+  #            tags$script(src="//d3js.org/d3.v4.js"),
+  #            tags$link(rel = 'stylesheet', type = 'text/css', href = 'custom.css')
+  #          ),
+  #          #sidebarPanel for data upload,
+  #          tags$div(id = "sidePanel", class = 'col-sm-4',
+  #                   tags$form(class = 'well',
+  #                             tags$div(class = 'row-fluid',
+  #                                      fileInput('file', 'Choose CSV file', accept=c('text/csv',
+  #                                                                                    'text/comma-separated-values,text/plain',
+  #                                                                                    '.csv'))),
+  #                             tags$div(class = 'row-fluid',
+  #                                      selectInput("select_data", "Select data", c("Single sample", "Three samples", "Thirty-six samples"))),
+  #                             tags$div(class = 'row-fluid',
+  #                                      checkboxInput('use_example', 'Use example data')),
+  #                             tags$div(class = 'row-fluid',
+  #                                      downloadLink('download_example', "Download example data"))
+  #                   ),
+  #                   tags$form(id = 'guide', class = 'well',
+  #                             tags$div(h4("Guide:"),
+  #                                      p(id = "step1", class = "inst", "1. Select Data"),
+  #                                      p(id = "step2", class = "inst", "2. Click and drag on the plot to identify the area with true peaks"),
+  #                                      p(id = "step3", class = "inst", "3. Click to identify peaks")),
+  #                             actionButton("goButton", "4. Process Data", width = '100%')
+  #                   )#,
+  #                   # tags$form(id = 'restart', class = 'well',
+  #                   #           actionButton("restartButton", "Start again", width = '100%'))
+  #          ),
+  #          mainPanel(
+  #            # textOutput("granular_version"),
+  #            tags$script(src="plotter.js"),
+  #            mastersizer_vis("mastersizer")
+  #          )
+  # ),
+  # tabPanel(title = "Output", value = "output",
+  #          shiny::dataTableOutput("longDataTable")
+  # ),
+  # tabPanel(title = "Summary", value = "summary"
+  # ),
+  # tabPanel(title = HTML("Plots</a></li><li><button id='restartButton' type='button' class='action-button shiny-bound-input navbar-button'>Start again</button></li>"),
+  #          value = "plots",
+  #          div(class = "center-button",
+  #              downloadButton("downloadPlot", "Download all fit plots as zip", class = "center-button")
+  #          )
+  # ),
+  # tabPanel(title = "About",
+  #          div(class = 'col-sm-4'),
+  #          div(class = 'col-sm-4',
+  #              includeMarkdown("about.md")
+  #          )
+  # )
+)
+  
