0a67cb1d52e4cca9109d2029a2cdd88a2a8b781a
Alex Whan <alexwhan@gmail.com>
2016-11-26 15:46:38 +1100

    removed shiny test dependencies and tests

--- tests/testthat/test_shiny.R
+++ tests/testthat/test_shiny.R
@@ -1,9 +1,9 @@
-library(shinytest)
-library(shinyjs)
-context("shinyapp")
-
-app <- shinytest::shinyapp$new("../../granular/shiny/granular/")
-
-test_that("get_value", {
-  expect_false(app$get_value("use_example"))
-})
+# library(shinytest)
+# library(shinyjs)
+# context("shinyapp")
+# 
+# app <- shinytest::shinyapp$new("../../granular/shiny/granular/")
+# 
+# test_that("get_value", {
+#   expect_false(app$get_value("use_example"))
+# })
