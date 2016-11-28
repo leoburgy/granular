8aae062422f654c582fbd6bb05a9add6089aa715
Alex Whan <alexwhan@gmail.com>
2016-11-28 16:42:47 +1100

    trying test with dependency

--- tests/testthat/test_shiny.R
+++ tests/testthat/test_shiny.R
@@ -1,9 +1,11 @@
 library(shinytest)
 library(shinyjs)
-context("shinyapp")
 
-app <- shinytest::shinyapp$new("../../granular/shiny/granular/")
-
-test_that("get_value", {
-  expect_false(app$get_value("use_example"))
-})
+if(!requireNamespace("shinytest", quietly = TRUE)) {
+  context("shinyapp")
+  app <- shinytest::shinyapp$new("../../granular/shiny/granular/")
+  
+  test_that("get_value", {
+    expect_false(app$get_value("use_example"))
+  })
+}
