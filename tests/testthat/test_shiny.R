e19a4b5efe6f8795c24f4ded63a2be9b0cdf66c3
Alex Whan <alexwhan@gmail.com>
2016-11-28 18:08:02 +1100

    kill me now

--- tests/testthat/test_shiny.R
+++ tests/testthat/test_shiny.R
@@ -1,6 +1,6 @@
 # library(shinytest)
 
-if(!require("shinytest", quietly = TRUE)) {
+if(require("shinytest", quietly = TRUE)) {
   context("shinyapp")
   app <- shinytest::shinyapp$new("../../granular/shiny/granular/")
   
