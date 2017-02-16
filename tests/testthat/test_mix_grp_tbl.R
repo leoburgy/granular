255555b50508c455615b165ab621164001de2c90
Whan, Alex (PI, Black Mountain) <Alex.Whan@csiro.au>
2017-02-16 11:30:18 +1100

    added test for mix_grp_tbl

---
+++ tests/testthat/test_mix_grp_tbl.R
@@ -0,0 +1,18 @@
+library(granular)
+library(dplyr)
+library(purrr)
+library(tidyr)
+context("mix_grp_tbl")
+
+data(package = 'granular')
+means <- c(C = 1, B = 5, A = 22)
+
+grp_tbl <- Dist %>% 
+  gather(sample, proportion, -size) %>% 
+  group_by(sample) %>% 
+  mix_grp_tbl(proportion, size, means)
+
+test_that("mix_grp_tbl is behaving", {
+  expect_equal(grp_tbl %>% unnest %>% filter(row_number() > 6) %>% data.frame %>% mutate_at(vars(sample), as.factor), 
+               ms3, tolerance = 1e-05)
+})
